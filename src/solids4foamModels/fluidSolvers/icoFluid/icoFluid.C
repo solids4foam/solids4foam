0d33b4d70012544aa6665f0498ad4582adcbbd3d
Philip Cardiff <philipcardiff@gmail.com>
2016-07-24 11:41:55 +0100

    Added fluid solvers

---
+++ src/solids4foamModels/fluidSolvers/icoFluid/icoFluid.C
@@ -0,0 +1,347 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+\*---------------------------------------------------------------------------*/
+
+#include "icoFluid.H"
+#include "volFields.H"
+#include "fvm.H"
+#include "fvc.H"
+#include "fvMatrices.H"
+#include "addToRunTimeSelectionTable.H"
+#include "findRefCell.H"
+#include "adjustPhi.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+namespace fluidSolvers
+{
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+defineTypeNameAndDebug(icoFluid, 0);
+addToRunTimeSelectionTable(fluidSolver, icoFluid, dictionary);
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+icoFluid::icoFluid(const fvMesh& mesh)
+:
+    fluidSolver(this->typeName, mesh),
+    U_
+    (
+        IOobject
+        (
+            "U",
+            runTime().timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    ),
+    p_
+    (
+        IOobject
+        (
+            "p",
+            runTime().timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    ),
+    gradp_(fvc::grad(p_)),
+    phi_
+    (
+        IOobject
+        (
+            "phi",
+            runTime().timeName(),
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::AUTO_WRITE
+        ),
+        fvc::interpolate(U_) & mesh.Sf()
+    ),
+    transportProperties_
+    (
+        IOobject
+        (
+            "transportProperties",
+            runTime().constant(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        )
+    ),
+    nu_(transportProperties_.lookup("nu")),
+    rho_(transportProperties_.lookup("rho"))
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+const volVectorField& icoFluid::U() const
+{
+    return U_;
+}
+
+
+const volScalarField& icoFluid::p() const
+{
+    return p_;
+}
+
+
+//- Patch viscous force (N/m2)
+// tmp<vectorField> icoFluid::patchViscousForce(const label patchID) const
+// {
+//     tmp<vectorField> tvF
+//     (
+//         new vectorField(mesh().boundary()[patchID].size(), vector::zero)
+//     );
+
+//     tvF() = rho_.value()*nu_.value()*U().boundaryField()[patchID].snGrad();
+
+// //     vectorField n = mesh().boundary()[patchID].nf();
+// //     tvF() -= n*(n&tvF());
+
+//     return tvF;
+// }
+
+// //- Patch pressure force (N/m2)
+// tmp<scalarField> icoFluid::patchPressureForce(const label patchID) const
+// {
+//     tmp<scalarField> tpF
+//     (
+//         new scalarField(mesh().boundary()[patchID].size(), 0)
+//     );
+
+//     tpF() = rho_.value()*p().boundaryField()[patchID];
+
+//     return tpF;
+// }
+
+// //- Patch viscous force (N/m2)
+// tmp<vectorField> icoFluid::faceZoneViscousForce
+// (
+//     const label zoneID,
+//     const label patchID
+// ) const
+// {
+//     vectorField pVF = patchViscousForce(patchID);
+
+//     tmp<vectorField> tvF
+//     (
+//         new vectorField(mesh().faceZones()[zoneID].size(), vector::zero)
+//     );
+//     vectorField& vF = tvF();
+
+//     const label patchStart =
+//         mesh().boundaryMesh()[patchID].start();
+
+//     forAll(pVF, i)
+//     {
+//         vF[mesh().faceZones()[zoneID].whichFace(patchStart + i)] =
+//             pVF[i];
+//     }
+
+//     // Parallel data exchange: collect pressure field on all processors
+//     reduce(vF, sumOp<vectorField>());
+
+
+//     return tvF;
+// }
+
+// //- Patch pressure force (N/m2)
+// tmp<scalarField> icoFluid::faceZonePressureForce
+// (
+//     const label zoneID,
+//     const label patchID
+// ) const
+// {
+//     scalarField pPF = patchPressureForce(patchID);
+
+//     tmp<scalarField> tpF
+//     (
+//         new scalarField(mesh().faceZones()[zoneID].size(), 0)
+//     );
+//     scalarField& pF = tpF();
+
+//     const label patchStart =
+//         mesh().boundaryMesh()[patchID].start();
+
+//     forAll(pPF, i)
+//     {
+//         pF[mesh().faceZones()[zoneID].whichFace(patchStart + i)] =
+//             pPF[i];
+//     }
+
+//     // Parallel data exchange: collect pressure field on all processors
+//     reduce(pF, sumOp<scalarField>());
+
+//     return tpF;
+// }
+
+// tmp<scalarField> icoFluid::faceZoneMuEff
+// (
+//     const label zoneID,
+//     const label patchID
+// ) const
+// {
+//     tmp<scalarField> tMuEff
+//     (
+//         new scalarField
+//         (
+//             mesh().faceZones()[zoneID].size(),
+//             rho_.value()*nu_.value()
+//         )
+//     );
+
+//     return tMuEff;
+// }
+
+
+void icoFluid::evolve()
+{
+    Info << "Evolving fluid model" << endl;
+
+    const fvMesh& mesh = this->mesh();
+
+    const int nCorr(readInt(fluidProperties().lookup("nCorrectors")));
+
+    const int nNonOrthCorr =
+        readInt(fluidProperties().lookup("nNonOrthogonalCorrectors"));
+
+    // Prepare for the pressure solution
+    label pRefCell = 0;
+    scalar pRefValue = 0.0;
+    setRefCell(p_, fluidProperties(), pRefCell, pRefValue);
+
+    if (mesh.moving())
+    {
+        // Make the fluxes relative
+        phi_ -= fvc::meshPhi(U_);
+    }
+
+    // Calculate CourantNo
+    {
+        scalar CoNum = 0.0;
+        scalar meanCoNum = 0.0;
+        scalar velMag = 0.0;
+
+        if (mesh.nInternalFaces())
+        {
+            surfaceScalarField SfUfbyDelta =
+                mesh.surfaceInterpolation::deltaCoeffs()*mag(phi_);
+
+            CoNum =
+                max(SfUfbyDelta/mesh.magSf()).value()
+               *runTime().deltaT().value();
+
+            meanCoNum =
+                (sum(SfUfbyDelta)/sum(mesh.magSf())).value()
+               *runTime().deltaT().value();
+
+            velMag = max(mag(phi_)/mesh.magSf()).value();
+        }
+
+        Info<< "Courant Number mean: " << meanCoNum
+            << " max: " << CoNum
+            << " velocity magnitude: " << velMag << endl;
+    }
+
+    // Construct momentum equation
+    fvVectorMatrix UEqn
+    (
+        fvm::ddt(U_)
+      + fvm::div(phi_, U_)
+      - fvm::laplacian(nu_, U_)
+    );
+
+    // Solve momentum equation
+    solve(UEqn == -gradp_);
+
+    // --- PISO loop
+
+    volScalarField rUA = 1.0/UEqn.A();
+
+    for (int corr = 0; corr < nCorr; corr++)
+    {
+        U_ = rUA*UEqn.H();
+        phi_ = (fvc::interpolate(U_) & mesh.Sf());
+
+        adjustPhi(phi_, U_, p_);
+
+        for (int nonOrth = 0; nonOrth <= nNonOrthCorr; nonOrth++)
+        {
+            // Construct pressure equation
+            fvScalarMatrix pEqn
+            (
+                fvm::laplacian(rUA, p_) == fvc::div(phi_)
+            );
+
+            // Solve pressure equation
+            pEqn.setReference(pRefCell, pRefValue);
+            pEqn.solve();
+
+            if (nonOrth == nNonOrthCorr)
+            {
+                phi_ -= pEqn.flux();
+            }
+        }
+
+        // Calculate continuity error
+        {
+            volScalarField contErr = fvc::div(phi_);
+
+            scalar sumLocalContErr = runTime().deltaT().value()*
+                mag(contErr)().weightedAverage(mesh.V()).value();
+
+            scalar globalContErr = runTime().deltaT().value()*
+                contErr.weightedAverage(mesh.V()).value();
+
+            Info<< "time step continuity errors : sum local = "
+                << sumLocalContErr << ", global = " << globalContErr << endl;
+        }
+
+        gradp_ = fvc::grad(p_);
+
+        U_ -= rUA*gradp_;
+        U_.correctBoundaryConditions();
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace fluidSolvers
+
+} // End namespace Foam
+
+// ************************************************************************* //
