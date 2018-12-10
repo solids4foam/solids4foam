f32c253fd50239c96543b9066d5b325306685a52
Philip Cardiff <philipcardiff@gmail.com>
2018-12-10 16:52:08 +0000

    thermoLinearElastic: update for reading T field from disk

--- src/solids4FoamModels/materialModels/mechanicalModel/mechanicalLaws/linearGeometryLaws/thermoLinearElastic/thermoLinearElastic.C
+++ src/solids4FoamModels/materialModels/mechanicalModel/mechanicalLaws/linearGeometryLaws/thermoLinearElastic/thermoLinearElastic.C
@@ -40,6 +40,49 @@ namespace Foam
 }
 
 
+// * * * * * * * * * * * * *  Private Data Members * * * * * * * * * * * * * //
+
+bool Foam::thermoLinearElastic::readTField()
+{
+    if (debug)
+    {
+        Info<< nl << "Attempting to read T from time = "
+            << mesh().time().timeName() << nl << endl;
+    }
+
+    // Only attempt to read the T field from disk once per time-step
+    if (curTimeIndex_ != mesh().time().timeIndex())
+    {
+        curTimeIndex_ = mesh().time().timeIndex();
+
+        IOobject Theader
+        (
+            "T",
+            mesh().time().timeName(),
+            mesh(),
+            IOobject::MUST_READ
+        );
+
+        if (Theader.headerOk())
+        {
+            Info<< nl << "Reading T field from time = " << mesh().time().timeName()
+                << nl << endl;
+
+            TPtr_.clear();
+            TPtr_.set(new volScalarField(Theader, mesh()));
+
+            TFieldWasReadFromDisk_ = true;
+
+            // Set the T field to write to disk: this allows a restart where the T
+            // field was only specified in the first time-step
+            TPtr_().writeOpt() = IOobject::AUTO_WRITE;
+        }
+    }
+
+    return TFieldWasReadFromDisk_;
+}
+
+
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
 // Construct from dictionary
@@ -54,8 +97,13 @@ Foam::thermoLinearElastic::thermoLinearElastic
     linearElastic(name, mesh, dict, nonLinGeom),
     alpha_(dict.lookup("alpha")),
     T0_(dict.lookup("T0")),
-    TPtr_(NULL)
-{}
+    TPtr_(),
+    TFieldWasReadFromDisk_(false),
+    curTimeIndex_(-1)
+{
+    // Check if the T field needs to be read from disk
+    readTField();
+}
 
 
 // * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
@@ -72,18 +120,26 @@ void Foam::thermoLinearElastic::correct(volSymmTensorField& sigma)
     // Calculate linear elastic stress
     linearElastic::correct(sigma);
 
-    if (TPtr_.valid())
-    {
-        // Add thermal stress component
-        sigma -= 3.0*K()*alpha_*(TPtr_() - T0_)*symmTensor(I);
-    }
-    else
+    if (mesh().foundObject<volScalarField>("T") && !TFieldWasReadFromDisk_)
     {
         // Lookup the temperature field from the solver
         const volScalarField& T = mesh().lookupObject<volScalarField>("T");
 
         // Add thermal stress component
         sigma -= 3.0*K()*alpha_*(T - T0_)*symmTensor(I);
+    }
+    else if (readTField())
+    {
+        // Add thermal stress component
+        sigma -= 3.0*K()*alpha_*(TPtr_() - T0_)*symmTensor(I);
+    }
+    else
+    {
+        FatalErrorIn(type() + "::correct(...)")
+            << "No T field found in memory or on disk. Make sure you have "
+            << "either specified a solidModel that solves for temperature "
+            << "or give the T field in at least the starting time "
+            << "directory" << abort(FatalError);
     }
 }
 
@@ -93,15 +149,7 @@ void Foam::thermoLinearElastic::correct(surfaceSymmTensorField& sigma)
     // Calculate linear elastic stress
     linearElastic::correct(sigma);
 
-    if (TPtr_.valid())
-    {
-        // Interpolate the volField temperature to the faces
-        const surfaceScalarField Tf = fvc::interpolate(TPtr_());
-
-        // Add thermal stress component
-        sigma -= 3.0*K()*alpha_*(Tf - T0_)*symmTensor(I);
-    }
-    else
+    if (mesh().foundObject<volScalarField>("T") && !TFieldWasReadFromDisk_)
     {
         // Lookup the temperature field from the solver
         const volScalarField& T = mesh().lookupObject<volScalarField>("T");
@@ -111,6 +159,22 @@ void Foam::thermoLinearElastic::correct(surfaceSymmTensorField& sigma)
 
         // Add thermal stress component
         sigma -= 3.0*K()*alpha_*(Tf - T0_)*symmTensor(I);
+    }
+    else if (readTField())
+    {
+        // Interpolate the volField temperature to the faces
+        const surfaceScalarField Tf = fvc::interpolate(TPtr_());
+
+        // Add thermal stress component
+        sigma -= 3.0*K()*alpha_*(Tf - T0_)*symmTensor(I);
+    }
+    else
+    {
+        FatalErrorIn(type() + "::correct(...)")
+            << "No T field found in memory or on disk. Make sure you have "
+            << "either specified a solidModel that solves for temperature "
+            << "or give the T field in at least the starting time "
+            << "directory" << abort(FatalError);
     }
 }
 
