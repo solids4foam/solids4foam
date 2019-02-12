7fd28ece167a67e6c2215988b1ac5d32dc96381e
Philip Cardiff <philipcardiff@gmail.com>
2019-02-12 21:18:53 +0000

    Added timeVaryingVelocityPointPatchVectorField

---
+++ src/solids4FoamModels/fluidModels/pointPatchFields/timeVaryingVelocity/timeVaryingVelocityPointPatchVectorField.C
@@ -0,0 +1,173 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "timeVaryingVelocityPointPatchVectorField.H"
+#include "pointPatchFields.H"
+#include "addToRunTimeSelectionTable.H"
+#include "polyMesh.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+timeVaryingVelocityPointPatchVectorField::
+timeVaryingVelocityPointPatchVectorField
+(
+    const pointPatch& p,
+    const DimensionedField<vector, pointMesh>& iF
+)
+:
+    fixedValuePointPatchVectorField(p, iF),
+    timeSeries_(),
+    curTimeIndex_(-1)
+{}
+
+
+timeVaryingVelocityPointPatchVectorField::
+timeVaryingVelocityPointPatchVectorField
+(
+    const pointPatch& p,
+    const DimensionedField<vector, pointMesh>& iF,
+    const dictionary& dict
+)
+:
+    fixedValuePointPatchVectorField(p, iF, dict),
+    timeSeries_(dict),
+    curTimeIndex_(-1)
+{
+    if (!dict.found("value"))
+    {
+        updateCoeffs();
+    }
+}
+
+
+timeVaryingVelocityPointPatchVectorField::
+timeVaryingVelocityPointPatchVectorField
+(
+    const timeVaryingVelocityPointPatchVectorField& ptf,
+    const pointPatch& p,
+    const DimensionedField<vector, pointMesh>& iF,
+#ifdef OPENFOAM
+    const pointPatchFieldMapper& mapper
+#else
+    const PointPatchFieldMapper& mapper
+#endif
+)
+:
+    fixedValuePointPatchVectorField(ptf, p, iF, mapper),
+    timeSeries_(ptf.timeSeries_),
+    curTimeIndex_(ptf.curTimeIndex_)
+{}
+
+
+timeVaryingVelocityPointPatchVectorField::
+timeVaryingVelocityPointPatchVectorField
+(
+    const timeVaryingVelocityPointPatchVectorField& ptf,
+    const DimensionedField<vector, pointMesh>& iF
+)
+:
+    fixedValuePointPatchVectorField(ptf, iF),
+    timeSeries_(ptf.timeSeries_),
+    curTimeIndex_(ptf.curTimeIndex_)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void timeVaryingVelocityPointPatchVectorField::autoMap
+(
+#ifdef OPENFOAM
+    const pointPatchFieldMapper& m
+#else
+    const PointPatchFieldMapper& m
+#endif
+)
+{
+    fixedValuePointPatchVectorField::autoMap(m);
+}
+
+
+void timeVaryingVelocityPointPatchVectorField::rmap
+(
+    const pointPatchField<vector>& ptf,
+    const labelList& addr
+)
+{
+    const timeVaryingVelocityPointPatchVectorField& oVptf =
+        refCast<const timeVaryingVelocityPointPatchVectorField>(ptf);
+
+    fixedValuePointPatchVectorField::rmap(oVptf, addr);
+}
+
+
+void timeVaryingVelocityPointPatchVectorField::updateCoeffs()
+{
+    if (this->updated())
+    {
+        return;
+    }
+
+    vector velocity = vector::zero;
+
+    if (curTimeIndex_ != db().time().timeIndex())
+    {
+        curTimeIndex_ = db().time().timeIndex();
+
+        // Lookup velocity from interpolation table
+        velocity = timeSeries_(db().time().timeOutputValue());
+    }
+
+    Field<vector>::operator=(velocity);
+
+    fixedValuePointPatchVectorField::updateCoeffs();
+}
+
+
+void timeVaryingVelocityPointPatchVectorField::write(Ostream& os) const
+{
+    pointPatchField<vector>::write(os);
+    timeSeries_.write(os);
+    writeEntry("value", os);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+makePointPatchTypeField
+(
+    pointPatchVectorField,
+    timeVaryingVelocityPointPatchVectorField
+);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
