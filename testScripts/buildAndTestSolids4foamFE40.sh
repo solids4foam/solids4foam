be8cbda2452943b610142c42b61159e26708f113
Philip Cardiff <philipcardiff@gmail.com>
2021-05-26 21:01:13 +0100

    buildAndTestSolids4foamFE40.sh: change docker image

--- testScripts/buildAndTestSolids4foamFE40.sh
+++ testScripts/buildAndTestSolids4foamFE40.sh
@@ -16,7 +16,7 @@ chmod -R o+w .
 # Create container
 CONTAINER="autobuild-solids4foam-foam-extend-4.0"
 solids4foam::createContainer "foam-extend-4.0" \
-    "philippic/foam-extend-4.0-ubuntu18.04.gfortran:latest" \
+    "philippic/foam-extend-4.0.ubuntu18.04.gfortran.python3:latest" \
     "${CONTAINER}" \
     "/home/app"
 
