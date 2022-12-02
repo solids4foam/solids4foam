39040a92138cbfe0f3fae106612e753844a3e261
scottlevie97 <78079249+scottlevie97@users.noreply.github.com>
2022-12-02 13:07:22 +0000

    Feature object volumetric strain (#17)
    * Add new function object to write differetnial stress
    
    * Add new function object to write volumetric strain
    
    * Update to read grad(D) when calculating volEpsilon
    
    * Remove differentialStresses from this branch
    
    * volumetricStrain: add compressionPositive flag
    
    Co-authored-by: Philip Cardiff <philip.cardiff@ucd.ie>

---
+++ src/solids4FoamModels/functionObjects/volumetricStrain/volumetricStrain.C
@@ -0,0 +1,146 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+\*----------------------------------------------------------------------------*/
+
+#include "volumetricStrain.H"
+#include "addToRunTimeSelectionTable.H"
+#include "volFields.H"
+#include "pointFields.H"
+#include "fvc.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(volumetricStrain, 0);
+
+    addToRunTimeSelectionTable
+    (
+        functionObject,
+        volumetricStrain,
+        dictionary
+    );
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+bool Foam::volumetricStrain::writeData()
+{
+    if (runTime_.outputTime() && mesh_.foundObject<volTensorField>("grad(D)"))
+    {
+        // Lookup the displacement gradient field
+        const volTensorField& gradD =
+            mesh_.lookupObject<volTensorField>("grad(D)");
+
+        // Calculate volumetric strain
+        volScalarField volEpsilon("volEpsilon", tr(gradD));
+
+        if (compressionPositive_)
+        {
+            volEpsilon = -volEpsilon;
+        }
+
+        // Write the field
+        Info<< name_ << ": writing volEpsilon" << endl;
+        volEpsilon.write();
+    }
+    else
+    {
+        Info<< name_ << ": grad(D) not found!" << endl;
+    }
+
+    return true;
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::volumetricStrain::volumetricStrain
+(
+    const word& name,
+    const Time& t,
+    const dictionary& dict
+)
+:
+    functionObject(name),
+    name_(name),
+    runTime_(t),
+    mesh_
+    (
+        runTime_.lookupObject<fvMesh>
+        (
+            dict.lookupOrDefault<word>("region", "region0")
+        )
+    ),
+    compressionPositive_
+    (
+        dict.lookupOrDefault("compressionPositive", false)
+    )
+{
+    Info<< "Creating " << this->name() << " function object" << endl;
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+bool Foam::volumetricStrain::start()
+{
+    if (runTime_.outputTime())
+    {
+        return writeData();
+    }
+
+    return true;
+}
+
+
+#if FOAMEXTEND
+bool Foam::volumetricStrain::execute(const bool forceWrite)
+#else
+bool Foam::volumetricStrain::execute()
+#endif
+{
+    if (runTime_.outputTime())
+    {
+        return writeData();
+    }
+
+    return true;
+}
+
+
+bool Foam::volumetricStrain::read(const dictionary& dict)
+{
+    return true;
+}
+
+#ifdef OPENFOAMESIORFOUNDATION
+bool Foam::volumetricStrain::write()
+{
+    return writeData();
+}
+#endif
+
+// ************************************************************************* //
