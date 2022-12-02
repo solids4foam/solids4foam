e9bfef29f2211d669186e01397ba56060ab196d9
Philip Cardiff <philip.cardiff@ucd.ie>
2022-12-02 12:26:13 +0000

    Compilation fix for principalStresses

--- src/solids4FoamModels/functionObjects/principalStresses/principalStressFields.C
+++ src/solids4FoamModels/functionObjects/principalStresses/principalStressFields.C
@@ -40,17 +40,13 @@ void Foam::calculateEigenValues
     vector& sigmaMinDir
 )
 {
-    // Multiply stress tensor by -1 if compression is positive
+    vector eValues = eigenValues(sigma);
+    const tensor eVectors = eigenVectors(sigma);
+
     if (compressionPositive)
     {
-        forAll(sigma, cellI)
-        {
-            sigma[cellI] = sigma[cellI]*(-1);
-        }
-    };
-
-    const vector eValues = eigenValues(sigma);
-    const tensor eVectors = eigenVectors(sigma);
+        eValues = -eValues;
+    }
 
     label iMax = -1;
     label iMid = -1;
