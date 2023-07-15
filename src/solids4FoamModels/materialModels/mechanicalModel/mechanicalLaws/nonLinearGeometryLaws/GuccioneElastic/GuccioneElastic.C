b90b7c64bb62364f7cf6b5fe6391a3c389542357
Philip Cardiff <philip.cardiff@ucd.ie>
2023-07-15 20:35:24 +0100

    GuccioneElastic: remove unused fields; update pressure

--- src/solids4FoamModels/materialModels/mechanicalModel/mechanicalLaws/nonLinearGeometryLaws/GuccioneElastic/GuccioneElastic.C
+++ src/solids4FoamModels/materialModels/mechanicalModel/mechanicalLaws/nonLinearGeometryLaws/GuccioneElastic/GuccioneElastic.C
@@ -67,19 +67,20 @@ Foam::GuccioneElastic::GuccioneElastic
         ),
         mesh
     ),
-    f0f0_("f0f0", sqr(f0_)),
-    s0_
+    f0f_
     (
         IOobject
         (
-            "s0",
+            "f0f",
             mesh.time().timeName(),
             mesh,
             IOobject::MUST_READ,
             IOobject::NO_WRITE
         ),
         mesh
-    )
+    ),
+    f0f0_("f0f0", sqr(f0_)),
+    f0f0f_("f0f0f", sqr(f0f_))
 {
     // Check that f0 and s0 are unit vectors
 
@@ -90,25 +91,24 @@ Foam::GuccioneElastic::GuccioneElastic
             << abort(FatalError);
     }
 
-    if (min(mag(mag(s0_.primitiveField()))) < SMALL)
+    if (min(mag(mag(f0f_.primitiveField()))) < SMALL)
     {
         FatalErrorIn("GuccioneElastic::GuccioneElastic()")
-            << "At least one s0 vector has a length of zero!"
+            << "At least one f0f vector has a length of zero!"
             << abort(FatalError);
     }
 
     // Normalise f0
     f0_ /= mag(f0_);
-
-    // Normalise s0
-    s0_ /= mag(s0_);
+    f0f_ /= mag(f0f_);
 
     // Re-calculate f0f0
     f0f0_ = sqr(f0_);
+    f0f0f_ = sqr(f0f_);
 
     // Store old F
     F().storeOldTime();
-    //Ff().storeOldTime();
+    Ff().storeOldTime();
 }
 
 
@@ -141,6 +141,27 @@ Foam::tmp<Foam::volScalarField> Foam::GuccioneElastic::impK() const
 }
 
 
+Foam::tmp<Foam::volScalarField> Foam::GuccioneElastic::bulkModulus() const
+{
+    return tmp<volScalarField>
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                "impK",
+                mesh().time().timeName(),
+                mesh(),
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+            mesh(),
+            bulkModulus_
+        )
+    );
+}
+
+
 void Foam::GuccioneElastic::correct(volSymmTensorField& sigma)
 {
     // Update the deformation gradient field
@@ -160,7 +181,7 @@ void Foam::GuccioneElastic::correct(volSymmTensorField& sigma)
 
     // Calculate the right Cauchy–Green deformation tensor
     const volSymmTensorField C(symm(F.T() & F));
-    
+
     // Calculate the Green-Lagrange strain
     const volSymmTensorField E(0.5*(C - I));
 
@@ -193,21 +214,81 @@ void Foam::GuccioneElastic::correct(volSymmTensorField& sigma)
     // Calculate the 2nd Piola-Kirchhoff stress
     const volSymmTensorField S(dQdE*0.5*k_*exp(Q));
 
-    // Convert the second Piola-Kirchhoff stress to the Cauchy stress
-    sigma = J*symm(F & S & F.T());
+    // Update the hydrostatic stress
+    updateSigmaHyd
+    (
+        0.5*bulkModulus_*(pow(J, 2.0) - 1.0)/J,
+        (4.0/3.0)*k_ + bulkModulus_
+    );
+
+    // Convert the second Piola-Kirchhoff stress to the Cauchy stress and add
+    // a hydrostatic pressure term
+    sigma = J*symm(F & S & F.T()) + sigmaHyd()*I;
 }
 
 
 void Foam::GuccioneElastic::correct(surfaceSymmTensorField& sigma)
 {
-    notImplemented("GuccioneElastic::correct(surfaceSymmTensorField& sigma)");
+    // Update the deformation gradient field
+    // Note: if true is returned, it means that linearised elasticity was
+    // enforced by the solver via the enforceLinear switch
+    if (updateF(sigma, mu_, bulkModulus_))
+    {
+        return;
+    }
+
+    // Take a reference to the deformation gradient to make the code easier to
+    // read
+    const surfaceTensorField& F = this->Ff();
+
+    // Calculate the Jacobian of the deformation gradient
+    const surfaceScalarField J(det(F));
+
+    // Calculate the right Cauchy–Green deformation tensor
+    const surfaceSymmTensorField C(symm(F.T() & F));
+
+    // Calculate the Green-Lagrange strain
+    const surfaceSymmTensorField E(0.5*(C - I));
+
+    // Calculate E . E
+    const surfaceSymmTensorField sqrE(symm(E & E));
+
+    // Calculate the invariants of E
+    const surfaceScalarField I1(tr(E));
+    const surfaceScalarField I2(0.5*(sqr(tr(E)) - tr(sqrE)));
+    const surfaceScalarField I4(E && f0f0f_);
+    const surfaceScalarField I5(sqrE && f0f0f_);
+
+    // Calculate Q
+    const surfaceScalarField Q
+    (
+        ct_*sqr(I1)
+      - 2.0*ct_*I2
+     + (cf_ - 2.0*cfs_ + ct_)*sqr(I4)
+     + 2.0*(cfs_ - ct_)*I5
+    );
+
+    // Calculate the derivative of Q wrt to E
+    const surfaceSymmTensorField dQdE
+    (
+        2.0*ct_*E
+      + 2.0*(cf_ - 2.0*cfs_ + ct_)*I4*f0f0f_
+      + 2.0*(cfs_ - ct_)*symm((E & f0f0f_) + (f0f0f_ & E))
+    );
+
+    // Calculate the 2nd Piola-Kirchhoff stress with penalty pressure term
+    const surfaceSymmTensorField S(dQdE*0.5*k_*exp(Q));
+
+    // Convert the second Piola-Kirchhoff stress to the Cauchy stress and add
+    // a hydrostatic pressure term
+    sigma = J*symm(F & S & F.T()) + 0.5*bulkModulus_*((pow(J, 2) - 1)/J)*I;
 }
 
 
 void Foam::GuccioneElastic::setRestart()
 {
     F().writeOpt() = IOobject::AUTO_WRITE;
-    //Ff().writeOpt() = IOobject::AUTO_WRITE;
+    Ff().writeOpt() = IOobject::AUTO_WRITE;
 }
 
 // ************************************************************************* //
