cce8ad8e8f8eec1019dd5daaea895dba33e47b51
iBatistic <ivan.batistic2@gmail.com>
2023-09-04 08:11:11 +0200

    GitHub action for solids4FoamChangeCopyright.sh

--- applications/scripts/solids4FoamChangeCopyright.sh
+++ applications/scripts/solids4FoamChangeCopyright.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
-# This script was adapted from foamChangeCopyright in foam-extend-4.1
+#------------------------------------------------------------------------------
+# License
+#     This file is part of solids4foam.
+#
+#     solids4foam is free software: you can redistribute it and/or modify it
+#     under the terms of the GNU General Public License as published by the
+#     Free Software Foundation, either version 3 of the License, or (at your
+#     option) any later version.
+#
+#     solids4foam is distributed in the hope that it will be useful, but
+#     WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with solids4foam.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Script
+#     solids4FoamChangeCopyright
+#     adapted from foamChangeCopyright in foam-extend-4.1
+#
+# Description
+#     Updates the header of .H and .C files.
+#
+#     To run the script on multiple files, use the following command:
+#
+#     $ find . \( -name "*.C" -o -name "*.H" \) | xargs -n 1 "./scriptPath.sh"
+#
+#------------------------------------------------------------------------------
+
+# Check if the correct number of arguments are provided
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <file>"
+    echo "Wrong number of arguments, expected 1 found $#"
+    exit 1
+fi
+
+# Check if the file exists
+if [[ ! -f "$1" ]]; then
+    echo "File not found: $1"
+    exit 1
+fi
 
 # Read original file
 orig_file=$(< "$1")
@@ -30,10 +71,10 @@ mod_file=$(< "$1")
 
 # Compare original and modified file
 if [ "$orig_file" != "$mod_file" ]; then
+    echo "File header does not match the expected header"
     echo "The header has been updated!"
     exit 1;
 else
-    echo "No changes have been made!"
+    echo "File header matches the expected header."
     exit 0;
 fi
-
