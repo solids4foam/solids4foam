2681f1ecf60b494dfa0756d37a01abec817f7a52
iBatistic <ivan.batistic2@gmail.com>
2023-09-08 22:16:54 +0200

    Replacing NamedEnum with Enum for OPENFOAM_COM

--- src/solids4FoamModels/solidModels/solidModel/nonLinearGeometry/nonLinearGeometry.C
+++ src/solids4FoamModels/solidModels/solidModel/nonLinearGeometry/nonLinearGeometry.C
@@ -1,19 +1,19 @@
 /*---------------------------------------------------------------------------*\
-License
-    This file is part of solids4foam.
+ License
+     This file is part of solids4foam.
 
-    solids4foam is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by the
-    Free Software Foundation, either version 3 of the License, or (at your
-    option) any later version.
+     solids4foam is free software: you can redistribute it and/or modify it
+     under the terms of the GNU General Public License as published by the
+     Free Software Foundation, either version 3 of the License, or (at your
+     option) any later version.
 
-    solids4foam is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
+     solids4foam is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with solids4foam.  If not, see <http://www.gnu.org/licenses/>.
+     You should have received a copy of the GNU General Public License
+     along with solids4foam.  If not, see <http://www.gnu.org/licenses/>.
 
 \*---------------------------------------------------------------------------*/
 
@@ -26,6 +26,18 @@ namespace Foam
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
+#ifdef OPENFOAM_COM
+const Foam::Enum
+<
+    Foam::nonLinearGeometry::nonLinearType
+>
+Foam::NonLinearGeometry::nonLinearNames_;
+({
+    {nonLinearType::LINEAR_GEOMETRY, "linearGeometry"},
+    {nonLinearType::UPDATED_LAGRANGIAN, "updatedLagrangian"},
+    {nonLinearType::TOTAL_LAGRANGIAN, "totalLagrangian"},
+});
+#else
 template<>
 const char*
 Foam::NamedEnum<Foam::nonLinearGeometry::nonLinearType, 3>::names[] =
@@ -37,7 +49,7 @@ Foam::NamedEnum<Foam::nonLinearGeometry::nonLinearType, 3>::names[] =
 
 const Foam::NamedEnum<Foam::nonLinearGeometry::nonLinearType, 3>
 Foam::nonLinearGeometry::nonLinearNames_;
-
+#endif
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
