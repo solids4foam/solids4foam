7ee0ac26288efef8b0e037c75ecc1696683cdaf4
Philip Cardiff <philip.cardiff@ucd.ie>
2024-01-09 13:11:04 +0000

    Aesthetic changes to solids4FoamScripts.sh

--- applications/scripts/solids4FoamScripts.sh
+++ applications/scripts/solids4FoamScripts.sh
@@ -125,7 +125,7 @@ function solids4Foam::convertCaseFormat()
         then
            echo "OpenFOAM.org specific: replacing 'uniform' with 'lineUniform' in system/sample"
            sed -i "s/type.*uniform;/type lineUniform;/g" "${CASE_DIR}"/system/sample
-           
+
            echo "OpenFOAM.org specific: replacing 'face' with 'lineFace' in system/sample"
            sed -i "s/type.*face;/type lineFace;/g" "${CASE_DIR}"/system/sample
         fi
@@ -308,7 +308,7 @@ function solids4Foam::convertCaseFormatFoamExtend()
         then
            echo "OpenFOAM.org specific: replacing 'lineUniform' with 'uniform' in system/sample"
            sed -i "s/type.*lineUniform;/type uniform;/g" "${CASE_DIR}"/system/sample
-           
+
            echo "OpenFOAM.org specific: replacing 'lineFace' with 'face' in system/sample"
            sed -i "s/type.*lineFace;/type face;/g" "${CASE_DIR}"/system/sample
         fi
@@ -371,13 +371,13 @@ function solids4Foam::convertCaseFormatFoamExtend()
         fi
     fi
 
-    # 11. Resolve sample post-processing path from foam-extend
+    # 11. Resolve sample post-processing path for foam-extend
     if  [[ -n $(find "${CASE_DIR}" -name plot.gnuplot) ]]
     then
         echo "Updating plot.gnuplot"
         sed -i "s|postProcessing/sample/|postProcessing/sets/|g" plot.gnuplot
     fi
-    
+
     # 12. Resolve sampleDict post-processing path for foam-extend
     if  [[ -n $(find "${CASE_DIR}" -name plot.gnuplot) ]]
     then
