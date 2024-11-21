2527f414f2d212264fbf5e5156d99383f1fd9a80
Philip Cardiff <philipcardiff@gmail.com>
2024-11-21 21:06:21 +0000

    gradGlobalQR implemented: works in parallel but seems slower: parallel communication approach to be checked

--- src/solids4FoamModels/solidModels/linGeomTotalDispSolid/higherOrderGrad/higherOrderGrad.C
+++ src/solids4FoamModels/solidModels/linGeomTotalDispSolid/higherOrderGrad/higherOrderGrad.C
@@ -452,7 +452,11 @@ void higherOrderGrad::makeGlobalCellStencils() const
 
 void higherOrderGrad::makeStencils() const
 {
-    Info<< "higherOrderGrad::makeStencils()" << endl;
+    //if (debug)
+    {
+        InfoInFunction
+            << "start" << endl;
+    }
 
     if (Pstream::parRun())
     {
@@ -561,7 +565,11 @@ void higherOrderGrad::makeStencils() const
         stencilsBoundaryFaces[cellI].shrink();
     }
 
-    Info<< "higherOrderGrad::makeStencils(): end" << endl;
+    //if (debug)
+    {
+        InfoInFunction
+            << "end" << endl;
+    }
 }
 
 
@@ -631,7 +639,11 @@ void higherOrderGrad::generateExponents
 
 void higherOrderGrad::calcQRCoeffs() const
 {
-    Info<< "higherOrderGrad::calcQRCoeffs()" << endl;
+    //if (debug)
+    {
+        InfoInFunction
+            << "start" << endl;
+    }
 
     if (QRInterpCoeffsPtr_ || QRGradCoeffsPtr_)
     {
@@ -882,7 +894,278 @@ void higherOrderGrad::calcQRCoeffs() const
        QRGradCoeffs[cellI].shrink();
     }
 
-    Info<< "higherOrderGrad::calcQRCoeffs(): end" << endl;
+    //if (debug)
+    {
+        InfoInFunction
+            << "end" << endl;
+    }
+}
+
+
+void higherOrderGrad::calcGlobalQRCoeffs() const
+{
+    //if (debug)
+    {
+        InfoInFunction
+            << "start" << endl;
+    }
+
+    if (QRInterpCoeffsPtr_ || QRGradCoeffsPtr_)
+    {
+        FatalErrorInFunction
+            << "Pointers already set!" << abort(FatalError);
+    }
+
+    const fvMesh& mesh = mesh_;
+
+    QRInterpCoeffsPtr_.set(new List<DynamicList<scalar>>(mesh.nCells()));
+    List<DynamicList<scalar>>& QRInterpCoeffs = *QRInterpCoeffsPtr_;
+
+    QRGradCoeffsPtr_.set(new List<DynamicList<vector>>(mesh.nCells()));
+    List<DynamicList<vector>>& QRGradCoeffs = *QRGradCoeffsPtr_;
+
+    // Refernces for brevity and efficiency
+    const vectorField& CI = mesh.C();
+
+    // Collect CI for off-processor cells in the stencils
+    Map<vector> globalCI;
+    requestGlobalStencilData(CI, globalCI);
+
+    // Calculate Taylor series exponents
+    // 1 for zero order, 4 for 1 order, 10 for second order, etc.
+    DynamicList<FixedList<label, 3>> exponents;
+    generateExponents(N_, exponents);
+    const label Np = exponents.size();
+    if (debug)
+    {
+        Info<< "Np = " << Np << endl;
+    }
+
+    // Precompute factorials up to N
+    List<scalar> factorials(N_ + 1, 1.0);
+    for (label n = 1; n <= N_; ++n)
+    {
+        factorials[n] = factorials[n - 1]*n;
+    }
+
+    List<DynamicList<scalar>> c(mesh.nCells());
+    List<DynamicList<scalar>> cx(mesh.nCells());
+    List<DynamicList<scalar>> cy(mesh.nCells());
+    List<DynamicList<scalar>> cz(mesh.nCells());
+
+    const List<labelList>& stencils = globalCellStencils();
+
+    forAll(stencils, localCellI)
+    {
+        const labelList& curStencil = stencils[localCellI];
+
+        // Find max distance in this stencil
+        scalar maxDist = 0.0;
+        forAll(curStencil, cI)
+        {
+            const label neiGlobalCellID = curStencil[cI];
+            scalar d;
+            if (globalCells_.isLocal(neiGlobalCellID))
+            {
+                const label neiLocalCellID =
+                    globalCells_.toLocal(neiGlobalCellID);
+
+                d = mag(CI[neiLocalCellID] - CI[localCellI]);
+            }
+            else
+            {
+                d = mag(globalCI[neiGlobalCellID] - CI[localCellI]);
+            }
+
+            maxDist = max(maxDist, d);
+        }
+
+        // Loop over neighbours and construct matrix Q
+        const label Nn = curStencil.size();
+
+        // Use matrix format from Eigen/Dense library
+        // Avoid initialisation to zero as we will set every entry below
+        Eigen::MatrixXd Q(Np, Nn);
+
+        // Check to avoid Eigen error
+        if (Nn < Np)
+        {
+            FatalErrorInFunction
+                << "Interpolation stencil needs to be bigger than the "
+                << "number of elements in Taylor order!"
+                << exit(FatalError);
+        }
+
+        // Loop over stencil points
+        for (label cI = 0; cI < Nn; ++cI)
+        {
+            const label neiGlobalCellID = curStencil[cI];
+            vector dx;
+            if (globalCells_.isLocal(neiGlobalCellID))
+            {
+                const label neiLocalCellID =
+                    globalCells_.toLocal(neiGlobalCellID);
+
+                dx = CI[neiLocalCellID] - CI[localCellI];
+            }
+            else
+            {
+                dx = globalCI[neiGlobalCellID] - CI[localCellI];
+            }
+
+            // Compute monomial values for each exponent
+            for (label p = 0; p < Np; ++p)
+            {
+                const FixedList<label, 3>& exponent = exponents[p];
+                const label i = exponent[0];
+                const label j = exponent[1];
+                const label k = exponent[2];
+
+               // Compute factorial denominator
+               const scalar factorialDenominator =
+                   factorials[i]*factorials[j]*factorials[k];
+
+               // Compute and assign monomial value with factorials
+               // Note: the order of the quadratic and higher terms may not be
+               // the same as the previous manual approach
+               Q(p, cI) =
+                   pow(dx.x(), i)*pow(dx.y(), j)*pow(dx.z(), k)
+                  /factorialDenominator;
+            }
+        }
+
+        Eigen::DiagonalMatrix<double, Eigen::Dynamic> W(Nn);
+        //W.setZero(); // no need to waste time initialising
+
+        for (label cI = 0; cI < Nn; cI++)
+        {
+            const label neiGlobalCellID = curStencil[cI];
+            scalar d;
+            if (globalCells_.isLocal(neiGlobalCellID))
+            {
+                const label neiLocalCellID =
+                    globalCells_.toLocal(neiGlobalCellID);
+
+                d = mag(CI[neiLocalCellID] - CI[localCellI]);
+            }
+            else
+            {
+                d = mag(globalCI[neiGlobalCellID] - CI[localCellI]);
+            }
+
+            // Smoothing length
+            const scalar dm = 2*maxDist;
+
+            // Weight using radially symmetric exponential function
+            const scalar sqrK = -pow(k_,2);
+            const scalar w =
+                (
+                    Foam::exp(pow(d/dm, 2)*sqrK) - Foam::exp(sqrK)
+                )/(1 - exp(sqrK));
+
+            W.diagonal()[cI] = w;
+        }
+
+        // Now when we have W and Q, next step is QR decomposition
+        const Eigen::DiagonalMatrix<double, Eigen::Dynamic> sqrtW =
+            W.diagonal().cwiseSqrt().asDiagonal();
+        const Eigen::MatrixXd Qhat =
+            Q.array().rowwise()*sqrtW.diagonal().transpose().array();
+
+        // B hat
+        const Eigen::DiagonalMatrix<double, Eigen::Dynamic>& Bhat =
+            sqrtW.diagonal().asDiagonal();
+
+        // QR decomposition
+        Eigen::HouseholderQR<Eigen::MatrixXd> qr(Qhat.transpose());
+
+        // Q and R matrices
+        const Eigen::MatrixXd O = qr.householderQ();
+        const Eigen::MatrixXd& R = qr.matrixQR().triangularView<Eigen::Upper>();
+
+        // Slice Rbar and Qbar, as we do not need full matrix
+        // Note: auto is a reference type here (Rbar, Qbar are not copied)
+        const auto Rbar = R.topLeftCorner(Np, Np);
+        const auto Qbar = O.leftCols(Np);
+
+        // Perform element-wise multiplication and convert to MatrixXd
+        const Eigen::MatrixXd QbarBhat =
+            (
+                Qbar.transpose().array().rowwise()
+               *Bhat.diagonal().transpose().array()
+            ).matrix();
+
+        // Solve to get A
+        // const Eigen::MatrixXd A =
+        //     Rbar.colPivHouseholderQr().solve(Qbar.transpose()*Bhat);
+        // Solve using the modified QbarBhat
+        const Eigen::MatrixXd A = Rbar.colPivHouseholderQr().solve(QbarBhat);
+
+        // To be aware of interpolation accuracy we need to control the
+        // condition number
+        if (calcConditionNumber_)
+        {
+            Eigen::JacobiSVD<Eigen::MatrixXd> svd
+            (
+                Rbar, Eigen::ComputeFullU | Eigen::ComputeFullV
+            );
+            Eigen::VectorXd singularValues = svd.singularValues();
+
+            conditionNumber()[localCellI] =
+                singularValues(0)
+               /(singularValues(singularValues.size() - 1) + VSMALL);
+        }
+
+        c[localCellI].setCapacity(A.cols());
+        cx[localCellI].setCapacity(A.cols());
+        cy[localCellI].setCapacity(A.cols());
+        cz[localCellI].setCapacity(A.cols());
+
+        Eigen::RowVectorXd cRow = A.row(0);
+        Eigen::RowVectorXd cxRow = A.row(1);
+        Eigen::RowVectorXd cyRow = A.row(2);
+        Eigen::RowVectorXd czRow = A.row(3);
+
+        for (label i = 0; i < A.cols(); ++i)
+        {
+            c[localCellI].append(cRow(i));
+            cx[localCellI].append(cxRow(i));
+            cy[localCellI].append(cyRow(i));
+            cz[localCellI].append(czRow(i));
+        }
+
+        c[localCellI].shrink();
+        cx[localCellI].shrink();
+        cy[localCellI].shrink();
+        cz[localCellI].shrink();
+    }
+
+    forAll(QRInterpCoeffs, localCellI)
+    {
+       const labelList& curStencil = stencils[localCellI];
+       const label Nn = curStencil.size();
+
+       QRInterpCoeffs[localCellI].setCapacity(Nn);
+       QRGradCoeffs[localCellI].setCapacity(Nn);
+
+       for (label I = 0; I < Nn; I++)
+       {
+           QRInterpCoeffs[localCellI].append(c[localCellI][I]);
+           QRGradCoeffs[localCellI].append
+           (
+               vector(cx[localCellI][I], cy[localCellI][I], cz[localCellI][I])
+           );
+       }
+
+       QRInterpCoeffs[localCellI].shrink();
+       QRGradCoeffs[localCellI].shrink();
+    }
+
+    //if (debug)
+    {
+        InfoInFunction
+            << "end" << endl;
+    }
 }
 
 
@@ -1351,7 +1634,14 @@ const List<DynamicList<scalar>>& higherOrderGrad::QRInterpCoeffs() const
 {
     if (!QRInterpCoeffsPtr_)
     {
-        calcQRCoeffs();
+        if (useGlobalStencils_)
+        {
+            calcGlobalQRCoeffs();
+        }
+        else
+        {
+            calcQRCoeffs();
+        }
     }
 
     return QRInterpCoeffsPtr_();
@@ -1362,7 +1652,14 @@ const List<DynamicList<vector>>& higherOrderGrad::QRGradCoeffs() const
 {
     if (!QRGradCoeffsPtr_)
     {
-        calcQRCoeffs();
+        if (useGlobalStencils_)
+        {
+            calcGlobalQRCoeffs();
+        }
+        else
+        {
+            calcQRCoeffs();
+        }
     }
 
     return QRGradCoeffsPtr_();
@@ -1527,7 +1824,14 @@ tmp<volTensorField> higherOrderGrad::grad(const volVectorField& D)
 {
     if (useQRDecomposition_)
     {
-        return gradQR(D);
+        if (useGlobalStencils_)
+        {
+            return gradGlobalQR(D);
+        }
+        else
+        {
+            return gradQR(D);
+        }
     }
     else
     {
@@ -1547,7 +1851,8 @@ tmp<volTensorField> higherOrderGrad::gradQR(const volVectorField& D)
 {
     if (debug)
     {
-        Info<< "higherOrderGrad::gradQR(...)" << endl;
+        InfoInFunction
+            << "start" << endl;
     }
 
     const fvMesh& mesh = mesh_;
@@ -1585,7 +1890,7 @@ tmp<volTensorField> higherOrderGrad::gradQR(const volVectorField& D)
 
         // Loop over stencil and multiply stencil cell values with
         // corresponding interpolation coefficient
-        for(label cI = 0; cI < Nn; cI++)
+        for (label cI = 0; cI < Nn; cI++)
         {
             if (cI < curStencil.size())
             {
@@ -1627,7 +1932,85 @@ tmp<volTensorField> higherOrderGrad::gradQR(const volVectorField& D)
 
     if (debug)
     {
-        Info<< "higherOrderGrad::gradQR(...): end" << endl;
+        InfoInFunction
+            << "end" << endl;
+    }
+
+    return tgradD;
+}
+
+
+tmp<volTensorField> higherOrderGrad::gradGlobalQR(const volVectorField& D)
+{
+    if (debug)
+    {
+        InfoInFunction
+            << "start" << endl;
+    }
+
+    const fvMesh& mesh = mesh_;
+
+    // Prepare the return field
+    tmp<volTensorField> tgradD
+    (
+        new volTensorField
+        (
+           IOobject
+           (
+               "grad(" + D.name() + ")",
+               mesh.time().timeName(),
+               mesh,
+               IOobject::NO_READ,
+               IOobject::AUTO_WRITE
+           ),
+           mesh,
+           dimensionedTensor("0", dimless, Zero),
+           "zeroGradient"
+        )
+    );
+    volTensorField& gradD = tgradD.ref();
+
+    const List<labelList>& stencils = globalCellStencils();
+    const List<DynamicList<vector>>& QRGradCoeffs = this->QRGradCoeffs();
+    const vectorField& DI = D;
+
+    // Collect DI for off-processor cells in the stencils
+    Map<vector> globalDI;
+    requestGlobalStencilData(DI, globalDI);
+
+    forAll(stencils, localCellI)
+    {
+        const labelList& curStencil = stencils[localCellI];
+        const label Nn = curStencil.size();
+
+        // Loop over stencil and multiply stencil cell values with
+        // corresponding interpolation coefficient
+        for (label cI = 0; cI < Nn; cI++)
+        {
+            const label neiGlobalCellI = curStencil[cI];
+
+            if (globalCells_.isLocal(neiGlobalCellI))
+            {
+                const label neiLocalCellI =
+                    globalCells_.toLocal(neiGlobalCellI);
+
+                gradD[localCellI] +=
+                    QRGradCoeffs[localCellI][cI]*DI[neiLocalCellI];
+            }
+            else
+            {
+                gradD[localCellI] +=
+                    QRGradCoeffs[localCellI][cI]*globalDI[neiGlobalCellI];
+            }
+        }
+    }
+
+    gradD.correctBoundaryConditions();
+
+    if (debug)
+    {
+        InfoInFunction
+            << "end" << endl;
     }
 
     return tgradD;
