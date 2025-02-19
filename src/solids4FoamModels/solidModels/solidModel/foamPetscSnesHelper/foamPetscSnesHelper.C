374281e0610b3ab6bc1a356e737fef6ec3b4c001
Philip Cardiff <philipcardiff@gmail.com>
2025-02-19 21:59:07 +0000

    Move generic coefficient insertion routines to foamPetscSnesHelper

--- src/solids4FoamModels/solidModels/solidModel/foamPetscSnesHelper/foamPetscSnesHelper.C
+++ src/solids4FoamModels/solidModels/solidModel/foamPetscSnesHelper/foamPetscSnesHelper.C
@@ -25,6 +25,7 @@ License
 #include "symmetryFvPatchFields.H"
 #include "symmetryPlaneFvPatchFields.H"
 #include "leastSquaresVectors.H"
+#include "fvm.H"
 
 // * * * * * * * * * * * * * * External Functions  * * * * * * * * * * * * * //
 
@@ -806,6 +807,395 @@ label foamPetscSnesHelper::InsertFvmGradIntoPETScMatrix
                 }
             }
         }
+    }
+
+    return 0;
+}
+
+
+label foamPetscSnesHelper::InsertFvmDivPhiUIntoPETScMatrix
+(
+    const volVectorField& U,
+    const surfaceScalarField& phi,
+    Mat jac,
+    const label rowOffset,
+    const label colOffset,
+    const label nScalarEqns,
+    const bool flipSign
+) const
+{
+    // Take references for efficiency and brevity
+    const fvMesh& mesh = mesh_;
+    const scalarField& phiI = phi;
+    const vectorField& UI = U;
+    const vectorField& SfI = mesh.Sf();
+    const scalarField& wI = mesh.weights();
+    const labelList& own = mesh.owner();
+    const labelList& nei = mesh.neighbour();
+    const scalar sign = flipSign ? -1 : 1;
+    tensor coeff = tensor::zero;
+
+    // Add segregated component of convection term
+    {
+        fvVectorMatrix UEqn
+        (
+          - fvm::div(phi, U, "jacobian-div(phi,U)")
+        );
+
+        UEqn.relax();
+
+        // Convert fvMatrix matrix to PETSc matrix
+        foamPetscSnesHelper::InsertFvMatrixIntoPETScMatrix
+        (
+            UEqn, jac, 0, 0, nScalarEqns
+        );
+    }
+
+    // Add additional term from div(phi,U) upwind linearisation
+    // (d/dU)(\sum phi*U = phi*I + w*Sf*Up
+    // for phi > 0, where phi = Sf*(w*Up + (1-w)*Un)
+    // The w*Sf*Up term is missing from fvm::div(phi(), U) as it requires a
+    // coupled solver. So we will add the w*Sf*Up term here
+    // Similary, for phi < 0, the neighbour coefficient is (1 - w)*Sf*Un
+
+    // Prepare coeff array
+    const label nCoeffCmpts = blockSize_*blockSize_;
+    PetscScalar values[nCoeffCmpts];
+    std::memset(values, 0, sizeof(values));
+
+    forAll(phiI, faceI)
+    {
+        // Local row ID
+        const label ownCellID = own[faceI];
+
+        // Local column ID
+        const label neiCellID = nei[faceI];
+
+        // Global block row ID
+        const label globalBlockRowI =
+            foamPetscSnesHelper::globalCells().toGlobal(ownCellID);
+
+        // Global block column ID
+        const label globalBlockColI =
+            foamPetscSnesHelper::globalCells().toGlobal(neiCellID);
+
+        if (phiI[faceI] > 0)
+        {
+            // Add w*Sf*Up to owner eqn
+            // coeff = sign*wI[faceI]*SfI[faceI]*UI[ownCellID];
+            coeff = sign*wI[faceI]*UI[ownCellID]*SfI[faceI];
+            for (label i = 0; i < (blockSize_ - 1); ++i)
+            {
+                for (label j = 0; j < (blockSize_ - 1); ++j)
+                {
+                    // Copy 3x3 (or 2x2 in 2-D) coeff into the top left of
+                    // the 4x4 (or 3x3 in 2-D) values matrix
+                    values[(i + rowOffset)*blockSize_ + j + colOffset] =
+                        coeff[i*3 + j];
+                }
+            }
+            CHKERRQ
+            (
+                MatSetValuesBlocked
+                (
+                    jac, 1, &globalBlockRowI, 1, &globalBlockRowI, values,
+                    ADD_VALUES
+                )
+            );
+
+            // Add (1 - w)*Sf*Up as nei to contribution to own eqn
+            // coeff = sign*(1.0 - wI[faceI])*SfI[faceI]*UI[ownCellID];
+            coeff = sign*(1.0 - wI[faceI])*UI[ownCellID]*SfI[faceI];
+            for (label i = 0; i < (blockSize_ - 1); ++i)
+            {
+                for (label j = 0; j < (blockSize_ - 1); ++j)
+                {
+                    // Copy 3x3 (or 2x2 in 2-D) coeff into the top left of
+                    // the 4x4 (or 3x3 in 2-D) values matrix
+                    values[(i + rowOffset)*blockSize_ + j + colOffset] =
+                        coeff[i*3 + j];
+                }
+            }
+            CHKERRQ
+            (
+                MatSetValuesBlocked
+                (
+                    jac, 1, &globalBlockRowI, 1, &globalBlockColI, values,
+                    ADD_VALUES
+                )
+            );
+
+            // Add -(1 - w)*Sf*Up to the neighbour diagonal
+            // Flip the sign
+            // coeff = -sign*(1.0 - wI[faceI])*SfI[faceI]*UI[ownCellID];
+            coeff = -sign*(1.0 - wI[faceI])*UI[ownCellID]*SfI[faceI];
+            for (label i = 0; i < (blockSize_ - 1); ++i)
+            {
+                for (label j = 0; j < (blockSize_ - 1); ++j)
+                {
+                    // Copy 3x3 (or 2x2 in 2-D) coeff into the top left of
+                    // the 4x4 (or 3x3 in 2-D) values matrix
+                    values[(i + rowOffset)*blockSize_ + j + colOffset] =
+                        coeff[i*3 + j];
+                }
+            }
+            CHKERRQ
+            (
+                MatSetValuesBlocked
+                (
+                    jac, 1, &globalBlockColI, 1, &globalBlockColI, values,
+                    ADD_VALUES
+                )
+            );
+
+            // Add -(1 - w)*Sf*Up as the own contribution to the nei eqn
+            // Flip the sign
+            // coeff = -sign*(1.0 - wI[faceI])*SfI[faceI]*UI[ownCellID];
+            coeff = -sign*(1.0 - wI[faceI])*UI[ownCellID]*SfI[faceI];
+            for (label i = 0; i < (blockSize_ - 1); ++i)
+            {
+                for (label j = 0; j < (blockSize_ - 1); ++j)
+                {
+                    // Copy 3x3 (or 2x2 in 2-D) coeff into the top left of
+                    // the 4x4 (or 3x3 in 2-D) values matrix
+                    values[(i + rowOffset)*blockSize_ + j + colOffset] =
+                        coeff[i*3 + j];
+                }
+            }
+            CHKERRQ
+            (
+                MatSetValuesBlocked
+                (
+                    jac, 1, &globalBlockColI, 1, &globalBlockRowI, values,
+                    ADD_VALUES
+                )
+            );
+        }
+        else
+        {
+            // Add w*Sf*Un to owner diagonal
+            // coeff = sign*wI[faceI]*SfI[faceI]*UI[neiCellID];
+            coeff = sign*wI[faceI]*UI[neiCellID]*SfI[faceI];
+            for (label i = 0; i < (blockSize_ - 1); ++i)
+            {
+                for (label j = 0; j < (blockSize_ - 1); ++j)
+                {
+                    // Copy 3x3 (or 2x2 in 2-D) coeff into the top left of
+                    // the 4x4 (or 3x3 in 2-D) values matrix
+                    values[(i + rowOffset)*blockSize_ + j + colOffset] =
+                        coeff[i*3 + j];
+                }
+            }
+            CHKERRQ
+            (
+                MatSetValuesBlocked
+                (
+                    jac, 1, &globalBlockRowI, 1, &globalBlockRowI, values,
+                    ADD_VALUES
+                )
+            );
+
+            // Add w*Sf*Un as nei to contribution to own eqn
+            // coeff = sign*wI[faceI]*SfI[faceI]*UI[neiCellID];
+            coeff = sign*wI[faceI]*UI[neiCellID]*SfI[faceI];
+            for (label i = 0; i < (blockSize_ - 1); ++i)
+            {
+                for (label j = 0; j < (blockSize_ - 1); ++j)
+                {
+                    // Copy 3x3 (or 2x2 in 2-D) coeff into the top left of
+                    // the 4x4 (or 3x3 in 2-D) values matrix
+                    values[(i + rowOffset)*blockSize_ + j + colOffset] =
+                        coeff[i*3 + j];
+                }
+            }
+            CHKERRQ
+            (
+                MatSetValuesBlocked
+                (
+                    jac, 1, &globalBlockRowI, 1, &globalBlockColI, values,
+                    ADD_VALUES
+                )
+            );
+
+            // Add -(1 - w)*Sf*Un to neighbour diagonal
+            // coeff = -sign*(1.0 - wI[faceI])*SfI[faceI]*UI[neiCellID];
+            coeff = -sign*(1.0 - wI[faceI])*UI[neiCellID]*SfI[faceI];
+            for (label i = 0; i < (blockSize_ - 1); ++i)
+            {
+                for (label j = 0; j < (blockSize_ - 1); ++j)
+                {
+                    // Copy 3x3 (or 2x2 in 2-D) coeff into the top left of
+                    // the 4x4 (or 3x3 in 2-D) values matrix
+                    values[(i + rowOffset)*blockSize_ + j + colOffset] =
+                        coeff[i*3 + j];
+                }
+            }
+            CHKERRQ
+            (
+                MatSetValuesBlocked
+                (
+                    jac, 1, &globalBlockColI, 1, &globalBlockColI, values,
+                    ADD_VALUES
+                )
+            );
+
+            // Add -w*Sf*Un as own to contribution to nei eqn
+            // coeff = -sign*wI[faceI]*SfI[faceI]*UI[neiCellID];
+            coeff = -sign*wI[faceI]*UI[neiCellID]*SfI[faceI];
+            for (label i = 0; i < (blockSize_ - 1); ++i)
+            {
+                for (label j = 0; j < (blockSize_ - 1); ++j)
+                {
+                    // Copy 3x3 (or 2x2 in 2-D) coeff into the top left of
+                    // the 4x4 (or 3x3 in 2-D) values matrix
+                    values[(i + rowOffset)*blockSize_ + j + colOffset] =
+                        coeff[i*3 + j];
+                }
+            }
+            CHKERRQ
+            (
+                MatSetValuesBlocked
+                (
+                    jac, 1, &globalBlockColI, 1, &globalBlockRowI, values,
+                    ADD_VALUES
+                )
+            );
+        }
+    }
+
+    // Boundary contribution for outlets
+    forAll(mesh.boundary(), patchI)
+    {
+        const fvPatchVectorField& pU = U.boundaryField()[patchI];
+        const fvPatch& patch = pU.patch();
+        const vectorField& pSf = patch.Sf();
+        //const scalarField& pphi = phi().boundaryField()[patchI];
+        const scalarField& pw = patch.weights();
+        const labelUList& fc = patch.faceCells();
+
+        if (patch.coupled())
+        {
+            notImplemented("div(phi,U) additional term for processors");
+        }
+        else if (patch.type() != "empty" && !pU.fixesValue())
+        {
+            forAll(fc, faceI)
+            {
+                // Local row ID
+                const label ownCellID = fc[faceI];
+
+                // Global block row ID
+                const label globalBlockRowI =
+                    foamPetscSnesHelper::globalCells().toGlobal(ownCellID);
+
+                // Add w*Sf*Up to owner eqn
+                coeff = sign*pw[faceI]*UI[ownCellID]*pSf[faceI];
+                for (label i = 0; i < (blockSize_ - 1); ++i)
+                {
+                    for (label j = 0; j < (blockSize_ - 1); ++j)
+                    {
+                        // Copy 3x3 (or 2x2 in 2-D) coeff into the top left of
+                        // the 4x4 (or 3x3 in 2-D) values matrix
+                        values[(i + rowOffset)*blockSize_ + j + colOffset] =
+                            coeff[i*3 + j];
+                    }
+                }
+                CHKERRQ
+                (
+                    MatSetValuesBlocked
+                    (
+                        jac, 1, &globalBlockRowI, 1, &globalBlockRowI, values,
+                        ADD_VALUES
+                    )
+                );
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+label foamPetscSnesHelper::InsertFvmDivUIntoPETScMatrix
+(
+    const volScalarField& p,
+    const volVectorField& U,
+    Mat jac,
+    const label rowOffset,
+    const label colOffset,
+    const label nScalarEqns,
+    const bool flipSign
+) const
+{
+    // Take references for efficiency and brevity
+    const fvMesh& mesh = mesh_;
+    const scalar sign = flipSign ? -1 : 1;
+
+    for (label cmptI = 0; cmptI < 3; ++cmptI)
+    {
+        if (nScalarEqns == 2 && cmptI == 2)
+        {
+            break;
+        }
+
+        fvScalarMatrix divUCoeffs(p, dimArea*dimPressure);
+
+        const vectorField& Sf = mesh.Sf();
+        const surfaceScalarField& weights = mesh.weights();
+        const scalarField& w = weights;
+
+        scalarField& upper = divUCoeffs.upper();
+        scalarField& lower = divUCoeffs.lower();
+
+        lower = w*Sf.component(cmptI);
+        upper = lower - Sf.component(cmptI);
+
+        divUCoeffs.negSumDiag();
+
+        // Boundary contributions
+        scalarField& diag = divUCoeffs.diag();
+        forAll(mesh.boundary(), patchI)
+        {
+            const fvPatchVectorField& pU = U.boundaryField()[patchI];
+            const fvPatch& patch = pU.patch();
+            const vectorField& Sf = patch.Sf();
+            const fvsPatchScalarField& pw = weights.boundaryField()[patchI];
+            const labelUList& fc = patch.faceCells();
+
+            const vectorField internalCoeffs(pU.valueInternalCoeffs(pw));
+
+            // Diag contribution
+            forAll(pU, faceI)
+            {
+                diag[fc[faceI]] -=
+                    sign*internalCoeffs[faceI][cmptI]*Sf[faceI][cmptI];
+            }
+
+            if (patch.coupled())
+            {
+                // Todo: add off-core coeffs
+                notImplemented("patch.coupled(): D-in-p");
+
+                // CoeffField<vector>::linearTypeField& pcoupleUpper =
+                //     bs.coupleUpper()[patchI].asLinear();
+                // CoeffField<vector>::linearTypeField& pcoupleLower =
+                //     bs.coupleLower()[patchI].asLinear();
+
+                // const vectorField pcl = -pw*Sf;
+                // const vectorField pcu = pcl + Sf;
+
+                // // Coupling  contributions
+                // pcoupleLower -= pcl;
+                // pcoupleUpper -= pcu;
+            }
+        }
+
+        // Insert component coeffs
+        // cmptI is the 1st column for Ux, 2nd for Uy, 3rd for Uz
+        foamPetscSnesHelper::InsertFvMatrixIntoPETScMatrix<scalar>
+        (
+            divUCoeffs, jac, rowOffset, colOffset + cmptI, 1
+        );
     }
 
     return 0;
