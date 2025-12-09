e8c603f62d65f845c54143c79375dc11051c9fbc
Philip Cardiff <philipcardiff@gmail.com>
2025-12-09 21:57:43 +0000

    solids4FoamScripts.sh: update faMeshDefinition

--- applications/scripts/solids4FoamScripts.sh
+++ applications/scripts/solids4FoamScripts.sh
@@ -101,6 +101,14 @@ function solids4Foam::convertCaseFormat()
         \mv "${CASE_DIR}"/constant/fluid/polyMesh/blockMeshDict "${CASE_DIR}"/system/fluid
     fi
 
+    # faMeshDefinition
+    if [[ -f "${CASE_DIR}"/constant/faMesh/faMeshDefinition ]]
+    then
+        echo "Moving constant/faMesh/faMeshDefinition to system/finite-area"
+        mkdir -p "${CASE_DIR}"/system/finite-area
+        \mv "${CASE_DIR}"/constant/faMesh/faMeshDefinition "${CASE_DIR}"/system/finite-area
+    fi
+
     # Replace the functions file
     if [[ -f "${CASE_DIR}"/system/functions ]]
     then
@@ -310,6 +318,14 @@ function solids4Foam::convertCaseFormatFoamExtend()
         echo "Moving system/fluid/blockMeshDict to constant/fluid/polyMesh"
         mkdir -p "${CASE_DIR}"/constant/fluid/polyMesh
         \mv "${CASE_DIR}"/system/fluid/blockMeshDict "${CASE_DIR}"/constant/fluid/polyMesh
+    fi
+
+    # faMeshDefinition
+    if [[ -f "${CASE_DIR}"/system/finite-area/faMeshDefinition ]]
+    then
+        echo "Moving system/finite-area/faMeshDefinition to constant/faMesh"
+        mkdir -p "${CASE_DIR}"/constant/polyMesh
+        \mv "${CASE_DIR}"/system/finite-area/faMeshDefinition "${CASE_DIR}"/constant/faMesh
     fi
 
     if [[ -f "${CASE_DIR}"/system/functions.foamextend ]]
