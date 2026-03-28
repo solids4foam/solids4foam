14ef4ef3e6f01293d12fd740ddffa10352817774
Philip Cardiff <philip.cardiff@ucd.ie>
2026-03-28 14:43:35 +0000

    Loosen 3dTube mean-force tolerance

--- tutorials/fluidSolidInteraction/3dTube/regressionTest.sh
+++ tutorials/fluidSolidInteraction/3dTube/regressionTest.sh
@@ -17,8 +17,8 @@ source "${SCRIPT_DIR}/../../../applications/scripts/solids4FoamScripts.sh"
 # Regression tolerances
 # ------------------------------------------------------------
 
-DISP_MAX_TOL=1e-6      # max displacement absolute tolerance
-FORCE_MEAN_TOL=2e-2    # mean force tolerance
+DISP_MAX_TOL=1e-6       # max displacement absolute tolerance
+FORCE_MEAN_TOL=2.5e-2   # mean force tolerance
 
 # Regression end time for the copied case only
 REG_END_TIME=0.0015
