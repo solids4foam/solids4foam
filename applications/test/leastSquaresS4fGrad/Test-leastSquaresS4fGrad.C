c8229e08fb5e54ffd60accba3889891b0cba7f1b
iBatistic <ivan.batistic2@gmail.com>
2026-06-23 08:23:11 +0200

    Added /Test-leastSquaresS4fGrad.C

---
+++ applications/test/leastSquaresS4fGrad/Test-leastSquaresS4fGrad.C
@@ -0,0 +1,585 @@
+/*---------------------------------------------------------------------------*\
+License
+    This file is part of solids4foam.
+
+    solids4foam is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    solids4foam is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with solids4foam.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    Test-leastSquaresS4fGrad
+
+Description
+    Tests scalar-field gradients against analytical fields on the case mesh.
+
+Author
+    Ivan Batistic, UCD
+    Philip Cardiff, UCD.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "boolIOList.H"
+#include "emptyFvPatch.H"
+#include "emptyFvPatchFields.H"
+#include "fixedDisplacementFvPatchVectorField.H"
+#include "fixedValueFvPatchFields.H"
+#include "IOmanip.H"
+#include "processorFvPatch.H"
+
+using namespace Foam;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+struct scalarGradTest
+{
+    word name;
+    scalar (*value)(const vector&);
+    vector (*grad)(const vector&);
+};
+
+
+struct errorStats
+{
+    scalar L2;
+    scalar LInf;
+    scalar boundaryL2;
+    scalar boundaryLInf;
+    label nBoundaryCells;
+};
+
+
+scalar mathematicalPi()
+{
+#ifdef OPENFOAM_NOT_EXTEND
+    return constant::mathematical::pi;
+#else
+    return mathematicalConstant::pi;
+#endif
+}
+
+
+scalar phiXValue(const vector& x)
+{
+    return x.x();
+}
+
+
+vector phiXGrad(const vector&)
+{
+    return vector(1.0, 0.0, 0.0);
+}
+
+
+scalar phiLinearValue(const vector& x)
+{
+    return 1.0 + x.x() + 2.0*x.y() - 0.5*x.z();
+}
+
+
+vector phiLinearGrad(const vector&)
+{
+    return vector(1.0, 2.0, -0.5);
+}
+
+
+scalar phiQuadraticValue(const vector& x)
+{
+    return sqr(x.x()) + 0.5*sqr(x.y()) + 0.25*sqr(x.z());
+}
+
+
+vector phiQuadraticGrad(const vector& x)
+{
+    return vector(2.0*x.x(), x.y(), 0.5*x.z());
+}
+
+
+scalar phiQuadraticCrossValue(const vector& x)
+{
+    return x.x()*x.y() + 0.25*x.y()*x.z();
+}
+
+
+vector phiQuadraticCrossGrad(const vector& x)
+{
+    return vector(x.y(), x.x() + 0.25*x.z(), 0.25*x.y());
+}
+
+
+scalar phiTrigonometricValue(const vector& x)
+{
+    const scalar pi(mathematicalPi());
+
+    return
+        Foam::sin(2.0*pi*x.x())*Foam::cos(3.0*pi*x.y())
+      + 0.25*Foam::sin(pi*(x.x() + x.y() + x.z()))
+      + 0.1*x.x()*x.y()*x.z();
+}
+
+
+vector phiTrigonometricGrad(const vector& x)
+{
+    const scalar pi(mathematicalPi());
+
+    const scalar common =
+        0.25*pi*Foam::cos(pi*(x.x() + x.y() + x.z()));
+
+    return vector
+    (
+        2.0*pi*Foam::cos(2.0*pi*x.x())*Foam::cos(3.0*pi*x.y())
+      + common
+      + 0.1*x.y()*x.z(),
+       -3.0*pi*Foam::sin(2.0*pi*x.x())*Foam::sin(3.0*pi*x.y())
+      + common
+      + 0.1*x.x()*x.z(),
+        common
+      + 0.1*x.x()*x.y()
+    );
+}
+
+
+vector activeGradient
+(
+    const vector& grad,
+    const Vector<label>& geometricD
+)
+{
+    vector activeGrad(grad);
+
+    for (direction cmpt=0; cmpt<vector::nComponents; cmpt++)
+    {
+        if (geometricD[cmpt] <= 0)
+        {
+            activeGrad[cmpt] = 0.0;
+        }
+    }
+
+    return activeGrad;
+}
+
+
+boolList boundaryOwnerCells(const fvMesh& mesh)
+{
+    boolList isBoundaryCell(mesh.nCells(), false);
+
+    forAll(mesh.boundary(), patchI)
+    {
+        const fvPatch& patch = mesh.boundary()[patchI];
+
+        if
+        (
+            isA<emptyFvPatch>(patch)
+         || isA<processorFvPatch>(patch)
+        )
+        {
+            continue;
+        }
+
+        const labelUList& faceCells = patch.faceCells();
+        forAll(faceCells, faceI)
+        {
+            isBoundaryCell[faceCells[faceI]] = true;
+        }
+    }
+
+    return isBoundaryCell;
+}
+
+
+volScalarField makeField
+(
+    const scalarGradTest& test,
+    const fvMesh& mesh,
+    const Time& runTime
+)
+{
+    wordList patchTypes
+    (
+        mesh.boundary().size(),
+        fixedValueFvPatchScalarField::typeName
+    );
+
+    forAll(mesh.boundary(), patchI)
+    {
+        const fvPatch& patch = mesh.boundary()[patchI];
+
+        if (patch.coupled())
+        {
+            patchTypes[patchI] = patch.type();
+        }
+        else if (isA<emptyFvPatch>(patch))
+        {
+            patchTypes[patchI] = emptyFvPatchScalarField::typeName;
+        }
+    }
+
+    volScalarField field
+    (
+        IOobject
+        (
+            test.name,
+            runTime.timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh,
+        dimensionedScalar("zero", dimless, 0.0),
+        patchTypes
+    );
+
+    scalarField& fieldI = field.primitiveFieldRef();
+    const volVectorField& C = mesh.C();
+
+    forAll(fieldI, cellI)
+    {
+        fieldI[cellI] = test.value(C[cellI]);
+    }
+
+    forAll(field.boundaryField(), patchI)
+    {
+        fvPatchScalarField& patchField = field.boundaryFieldRef()[patchI];
+
+        if
+        (
+            patchField.coupled()
+         || isA<emptyFvPatchScalarField>(patchField)
+        )
+        {
+            continue;
+        }
+
+        scalarField values(patchField.size(), 0.0);
+        const vectorField& Cf = patchField.patch().Cf();
+
+        forAll(values, faceI)
+        {
+            values[faceI] = test.value(Cf[faceI]);
+        }
+
+        patchField == values;
+    }
+
+    field.correctBoundaryConditions();
+
+    return field;
+}
+
+
+volVectorField makeExactGradField
+(
+    const scalarGradTest& test,
+    const fvMesh& mesh,
+    const Time& runTime
+)
+{
+    volVectorField exactGrad
+    (
+        IOobject
+        (
+            "gradExact_" + test.name,
+            runTime.timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh,
+        dimensionedVector("zero", dimless/dimLength, vector::zero)
+    );
+
+    vectorField& exactGradI = exactGrad.primitiveFieldRef();
+    const volVectorField& C = mesh.C();
+    const Vector<label> geometricD(mesh.geometricD());
+
+    forAll(exactGradI, cellI)
+    {
+        exactGradI[cellI] = activeGradient(test.grad(C[cellI]), geometricD);
+    }
+
+    forAll(exactGrad.boundaryField(), patchI)
+    {
+        fvPatchVectorField& patchField =
+            exactGrad.boundaryFieldRef()[patchI];
+
+        if
+        (
+            patchField.coupled()
+         || isA<emptyFvPatchVectorField>(patchField)
+        )
+        {
+            continue;
+        }
+
+        vectorField values(patchField.size(), vector::zero);
+        const vectorField& Cf = patchField.patch().Cf();
+
+        forAll(values, faceI)
+        {
+            values[faceI] =
+                activeGradient(test.grad(Cf[faceI]), geometricD);
+        }
+
+        patchField == values;
+    }
+
+    return exactGrad;
+}
+
+
+boolIOList makeUseBoundaryFaceValues
+(
+    const volScalarField& field,
+    const Time& runTime,
+    const volVectorField& D
+)
+{
+    const fvMesh& mesh = field.mesh();
+    boolList useBoundaryFaceValuesList(mesh.boundary().size(), false);
+
+    forAll(mesh.boundary(), patchI)
+    {
+        if
+        (
+            isA<fixedDisplacementFvPatchVectorField>
+            (
+                D.boundaryField()[patchI]
+            )
+        )
+        {
+            useBoundaryFaceValuesList[patchI] = true;
+        }
+    }
+
+    return boolIOList
+    (
+        IOobject
+        (
+            "useBoundaryFaceValues_" + field.name(),
+            runTime.constant(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        useBoundaryFaceValuesList
+    );
+}
+
+
+errorStats calcErrorStats
+(
+    const volScalarField& error,
+    const boolList& isBoundaryCell
+)
+{
+    scalar LInf = 0.0;
+    scalar sumSqrError = 0.0;
+    label nCells = 0;
+
+    scalar boundaryLInf = 0.0;
+    scalar boundarySumSqrError = 0.0;
+    label nBoundaryCells = 0;
+
+    const scalarField& errorI = error.primitiveField();
+
+    forAll(errorI, cellI)
+    {
+        const scalar e = errorI[cellI];
+
+        LInf = max(LInf, mag(e));
+        sumSqrError += magSqr(e);
+        nCells++;
+
+        if (isBoundaryCell[cellI])
+        {
+            boundaryLInf = max(boundaryLInf, mag(e));
+            boundarySumSqrError += magSqr(e);
+            nBoundaryCells++;
+        }
+    }
+
+    reduce(LInf, maxOp<scalar>());
+    reduce(sumSqrError, sumOp<scalar>());
+    reduce(nCells, sumOp<label>());
+
+    reduce(boundaryLInf, maxOp<scalar>());
+    reduce(boundarySumSqrError, sumOp<scalar>());
+    reduce(nBoundaryCells, sumOp<label>());
+
+    errorStats stats;
+    stats.L2 =
+        Foam::sqrt(sumSqrError/max(scalar(nCells), VSMALL));
+    stats.LInf = LInf;
+    stats.boundaryL2 =
+        Foam::sqrt
+        (
+            boundarySumSqrError/max(scalar(nBoundaryCells), VSMALL)
+        );
+    stats.boundaryLInf = boundaryLInf;
+    stats.nBoundaryCells = nBoundaryCells;
+
+    return stats;
+}
+
+
+errorStats testGrad
+(
+    const scalarGradTest& test,
+    const fvMesh& mesh,
+    const Time& runTime,
+    const boolList& isBoundaryCell,
+    const volVectorField& D,
+    const bool writeFields
+)
+{
+    volScalarField field(makeField(test, mesh, runTime));
+    volVectorField exactGrad(makeExactGradField(test, mesh, runTime));
+    boolIOList useBoundaryFaceValuesList
+    (
+        makeUseBoundaryFaceValues
+        (
+            field,
+            runTime,
+            D
+        )
+    );
+
+    tmp<volVectorField> tGrad(fvc::grad(field));
+    volVectorField grad
+    (
+        IOobject
+        (
+            "grad_" + test.name,
+            runTime.timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        tGrad()
+    );
+
+    volScalarField error
+    (
+        IOobject
+        (
+            "magGradError_" + test.name,
+            runTime.timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mag(grad - exactGrad)
+    );
+
+    if (writeFields)
+    {
+        field.write();
+        grad.write();
+        exactGrad.write();
+        error.write();
+    }
+
+    return calcErrorStats(error, isBoundaryCell);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::addBoolOption
+    (
+        "writeFields",
+        "Write analytical fields, gradients and error fields"
+    );
+    #include "setRootCase.H"
+
+    const bool writeFields = args.found("writeFields");
+
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    volVectorField D
+    (
+        IOobject
+        (
+            "D",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh
+    );
+
+    const boolList isBoundaryCell(boundaryOwnerCells(mesh));
+
+    const scalarGradTest tests[] =
+    {
+        {"phi_x", phiXValue, phiXGrad},
+        {"phi_linear", phiLinearValue, phiLinearGrad},
+        {"phi_quadratic", phiQuadraticValue, phiQuadraticGrad},
+        {"phi_quadratic_cross", phiQuadraticCrossValue, phiQuadraticCrossGrad},
+        {"phi_trigonometric", phiTrigonometricValue, phiTrigonometricGrad}
+    };
+    const label nTests = 5;
+    const label fieldWidth = 24;
+    const label valueWidth = 16;
+    const string headerPadding(fieldWidth - 5, ' ');
+
+    Info<< nl
+        << "Testing fvc::grad for scalar analytical fields" << nl
+        << "The active gradScheme is read from system/fvSchemes." << nl
+        << nl
+        << "field" << headerPadding.c_str()
+        << setw(valueWidth) << "L2"
+        << setw(valueWidth) << "LInf"
+        << setw(valueWidth) << "boundaryL2"
+        << setw(valueWidth) << "boundaryLInf"
+        << nl;
+
+    for (label testI = 0; testI < nTests; testI++)
+    {
+        const errorStats stats =
+            testGrad
+            (
+                tests[testI],
+                mesh,
+                runTime,
+                isBoundaryCell,
+                D,
+                writeFields
+            );
+
+        const word& fieldName = tests[testI].name;
+        const string fieldPadding
+        (
+            max(fieldWidth - label(fieldName.size()), label(1)),
+            ' '
+        );
+
+        Info<< fieldName
+            << fieldPadding.c_str()
+            << setw(valueWidth) << stats.L2
+            << setw(valueWidth) << stats.LInf
+            << setw(valueWidth) << stats.boundaryL2
+            << setw(valueWidth) << stats.boundaryLInf
+            << nl;
+    }
+
+    Info<< nl << "End" << nl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
--- tutorials/solids/linearElasticity/ellipticPlate/regressionTest.sh
+++ tutorials/solids/linearElasticity/ellipticPlate/regressionTest.sh
@@ -17,10 +17,10 @@ fi
 # solution extrema on the default mesh.
 # ============================================================
 
-EPS_MIN=3.8e-5
-EPS_MAX=4.1e-5
-SIGMA_MIN=9.65e6
-SIGMA_MAX=9.75e6
+EPS_MIN=5.84e-5
+EPS_MAX=5.86e-5
+SIGMA_MIN=1.41e7
+SIGMA_MAX=1.43e7
 
 SOLVER_LOGFILE="log.solids4Foam"
 ALLRUN_LOGFILE="log.Allrun"
@@ -30,10 +30,17 @@ APPROACHES=(
     petscSnes
 )
 
+# Settings for Test-leastSquaresS4fGrad test
+GRADIENT_REL_TOL=1e-5
+GRADIENT_ABS_TOL=1e-10
+GRADIENT_SERIAL_LOGFILE="log.Test-leastSquaresS4fGrad.serial"
+GRADIENT_PARALLEL_LOGFILE="log.Test-leastSquaresS4fGrad.parallel"
+
 echo "============================================================"
 echo "ellipticPlate regression test"
 echo "Max epsilonEq in [${EPS_MIN}, ${EPS_MAX}]"
 echo "Max sigmaEq   in [${SIGMA_MIN}, ${SIGMA_MAX}]"
+echo "Gradient relative tolerance: ${GRADIENT_REL_TOL}"
 echo "============================================================"
 echo
 
@@ -64,6 +71,7 @@ done
 
 if [ "$CHECK_ONLY" = false ]; then
     prepare_case
+    rm -f "${CASE_DIR}/${GRADIENT_SERIAL_LOGFILE}" "${CASE_DIR}/${GRADIENT_PARALLEL_LOGFILE}"
 else
     echo "Running in check-only mode: skipping Allclean and Allrun"
 fi
@@ -111,6 +119,106 @@ check_solver_extrema() {
     return "${failures}"
 }
 
+check_gradient_log() {
+    local logfile="$1"
+    local mode="$2"
+    local failures=0
+
+    check_gradient_reference_field "${logfile}" "${mode}" phi_x 0 0 0 0 \
+        || failures=$((failures + 1))
+    check_gradient_reference_field "${logfile}" "${mode}" phi_linear 0 0 0 0 \
+        || failures=$((failures + 1))
+    check_gradient_reference_field "${logfile}" "${mode}" \
+        phi_quadratic 0.0449495 0.302343 0.078008 0.302343 \
+        || failures=$((failures + 1))
+    check_gradient_reference_field "${logfile}" "${mode}" \
+        phi_quadratic_cross 0.00882968 0.0421144 0.0142969 0.0421144 \
+        || failures=$((failures + 1))
+    check_gradient_reference_field "${logfile}" "${mode}" \
+        phi_trigonometric 1.56111 5.49017 1.86558 5.49017 \
+        || failures=$((failures + 1))
+
+    return "${failures}"
+}
+
+extract_gradient_row() {
+    local logfile="$1"
+    local field="$2"
+
+    awk -v field="${field}" \
+        '$1 == field && NF >= 5 {print $2, $3, $4, $5; exit}' \
+        "${logfile}" 2>/dev/null || true
+}
+
+check_gradient_reference_field() {
+    local logfile="$1"
+    local mode="$2"
+    local field="$3"
+    local ref_l2="$4"
+    local ref_linf="$5"
+    local ref_boundary_l2="$6"
+    local ref_boundary_linf="$7"
+    local row
+    local l2 linf boundary_l2 boundary_linf
+
+    row=$(extract_gradient_row "${logfile}" "${field}")
+    if [[ -z "${row}" ]]; then
+        echo "FAIL: Could not extract ${mode} gradient norms for ${field}"
+        return 1
+    fi
+
+    IFS=' ' read -r l2 linf boundary_l2 boundary_linf <<< "${row}"
+    if awk -v l2="${l2}" -v linf="${linf}" \
+        -v boundary_l2="${boundary_l2}" \
+        -v boundary_linf="${boundary_linf}" \
+        -v ref_l2="${ref_l2}" -v ref_linf="${ref_linf}" \
+        -v ref_boundary_l2="${ref_boundary_l2}" \
+        -v ref_boundary_linf="${ref_boundary_linf}" \
+        -v rel_tol="${GRADIENT_REL_TOL}" \
+        -v abs_tol="${GRADIENT_ABS_TOL}" '
+        function mag(value) {return value < 0 ? -value : value}
+        function is_close(value, reference) {
+            return mag(value - reference) <= abs_tol + rel_tol*mag(reference)
+        }
+        BEGIN {
+            exit !(is_close(l2, ref_l2) && is_close(linf, ref_linf) &&
+                is_close(boundary_l2, ref_boundary_l2) &&
+                is_close(boundary_linf, ref_boundary_linf))
+        }'
+    then
+        printf "PASS: %s %s: [%s] expected [%.8g %.8g %.8g %.8g]\n" \
+            "${mode}" "${field}" "${row}" \
+            "${ref_l2}" "${ref_linf}" "${ref_boundary_l2}" "${ref_boundary_linf}"
+        return 0
+    fi
+
+    printf "FAIL: %s %s: [%s] expected [%.8g %.8g %.8g %.8g]\n" \
+        "${mode}" "${field}" "${row}" \
+        "${ref_l2}" "${ref_linf}" "${ref_boundary_l2}" "${ref_boundary_linf}"
+    return 1
+}
+
+run_gradient_tests() {
+    if ! command -v Test-leastSquaresS4fGrad >/dev/null 2>&1; then
+        echo "FAIL: Test-leastSquaresS4fGrad is not available"
+        return 1
+    fi
+
+    prepare_case
+
+    (
+        cd "${CASE_DIR}"
+        solids4Foam::runApplication -o -s gradient blockMesh >/dev/null 2>&1
+        sed -E -i.bak 's/^[[:space:]]*default[[:space:]]+none;/        default         leastSquaresS4f;/' system/fvSchemes
+        rm -f system/fvSchemes.bak
+        solids4Foam::runApplication -o -s serial Test-leastSquaresS4fGrad >/dev/null 2>&1
+
+        solids4Foam::runApplication -o -s gradient decomposePar -force >/dev/null 2>&1
+        solids4Foam::runParallel -o -s parallel -n 2 \
+            Test-leastSquaresS4fGrad >/dev/null 2>&1
+    )
+}
+
 failures=0
 
 if [ "$CHECK_ONLY" = false ]; then
@@ -132,6 +240,19 @@ if [ "$CHECK_ONLY" = false ]; then
             failures=$((failures + 1))
         fi
     done
+
+    echo
+    echo "Testing leastSquaresS4f gradients in serial and parallel"
+    if run_gradient_tests; then
+        if ! check_gradient_log "${CASE_DIR}/${GRADIENT_SERIAL_LOGFILE}" serial; then
+            failures=$((failures + 1))
+        fi
+        if ! check_gradient_log "${CASE_DIR}/${GRADIENT_PARALLEL_LOGFILE}" parallel; then
+            failures=$((failures + 1))
+        fi
+    else
+        failures=$((failures + 1))
+    fi
 else
     if solids4Foam::regressionCaseSkipped "${CASE_DIR}/${ALLRUN_LOGFILE}"; then
         echo "Skipping regression checks because the tutorial skipped in this environment"
@@ -139,6 +260,12 @@ else
     fi
 
     if ! check_solver_extrema "check-only"; then
+        failures=$((failures + 1))
+    fi
+    if ! check_gradient_log "${CASE_DIR}/${GRADIENT_SERIAL_LOGFILE}" serial; then
+        failures=$((failures + 1))
+    fi
+    if ! check_gradient_log "${CASE_DIR}/${GRADIENT_PARALLEL_LOGFILE}" parallel; then
         failures=$((failures + 1))
     fi
 fi
